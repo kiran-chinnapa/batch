08504fbd6b7e01f9292e6e0e4ebc1e4b282de986
kiran-chinnapa <kiranms.chinnapa@gmail.com>
2022-06-09 00:46:38 -0400

    using jdbc template in junit test

--- src/test/java/com/example/demo/BatchApplicationTest.java
+++ src/test/java/com/example/demo/BatchApplicationTest.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 
 @SpringBootTest
 class BatchApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
 
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Test
+	public void testDbRepo(){
+		List list = jdbcTemplate.queryForList("select * from batch_job_instance");
+		System.out.println("jdbctemplate:"+list.get(0).toString());
+	}
 }
